0b4a443466c121e7bc4b20a9c59a003bb9a18392
Aaron <aws.clemens@gmail.com>
2020-09-26 11:55:44 -0600

    added Header Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // App.js
 
-// Functions from the React Library
+// imports
 import React, { Component } from 'react';
-// CSS for the 'App' component
+import Header from './Components/Header.js';
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>xkcd</h1>
+        <Header />
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+// Header.js
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="wrapper">
+                    <h1>xkcd</h1>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
