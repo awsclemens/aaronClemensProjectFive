a9846a39802e2aceac0cadad39e961527b34cf70
Aaron <aws.clemens@gmail.com>
2020-09-27 15:46:52 -0600

    overlay now appears on click on enter press

--- src/Components/Comic.js
+++ src/Components/Comic.js
@@ -1,23 +1,42 @@
 // Comic.js
-import React from 'react';
+import React,{ Component } from 'react';
 import months from '../modules/monthsList.js';
 
-// Function Component
-const Comic = ({latestNum, comicNum, title, url, transcript, year, month, day, bonusText}) => {
-    return(
-        <div className="displayComic" key={comicNum}>
-            {comicNum === latestNum ? <h2>Latest Comic: #{comicNum}</h2> : <h2>Comic: #{comicNum}</h2>}
-            <p><time dateTime={`${year}-${month}-${day}`}>{months[parseInt(month) - 1]} {day}, {year}</time></p>
-            <h3>{title}</h3>
-            <div className="imageContainer" tabIndex='0'>
-                {transcript !== "" ? <img src={url} alt={transcript}></img> : <img src={url} alt="A Comic by Randall Patrick Munroe. Transcript unavailable at this time"></img> }
-                <div className="overlay">
-                    <p>{bonusText}</p>
+class Comic  extends Component {
+
+    // display overlay with its bonus text on mouse click
+    handleClick() {
+        const overlay = document.getElementsByClassName('overlay');
+        console.log(overlay[0].style.display);
+        return(overlay[0].style.display === "" ? overlay[0].style.display = "block" : overlay[0].style.display = "");
+    }
+
+    // display overlay with its bonus text on "Enter" key, accessibilty
+    handleEnter(e) {
+        if(e.key === 'Enter') {
+            const overlay = document.getElementsByClassName('overlay');
+            console.log(overlay[0].style.display);
+            return(overlay[0].style.display === "" ? overlay[0].style.display = "block" : overlay[0].style.display = "");
+        }
+    }
+
+    render(){
+        const {latestNum, comicNum, title, url, transcript, year, month, day, bonusText} = this.props
+        return(
+            <div className="displayComic" key={comicNum}>
+                {comicNum === latestNum ? <h2>Latest Comic: #{comicNum}</h2> : <h2>Comic: #{comicNum}</h2>}
+                <p><time dateTime={`${year}-${month}-${day}`}>{months[parseInt(month) - 1]} {day}, {year}</time></p>
+                <h3>{title}</h3>
+                <div className="imageContainer" tabIndex='0' onClick={this.handleClick} onKeyPress={this.handleEnter}>
+                    {transcript !== "" ? <img src={url} alt={transcript}></img> : <img src={url} alt="A Comic by Randall Patrick Munroe. Transcript unavailable at this time"></img> }
+                    <div className="overlay">
+                        <p>{bonusText}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        
-    );
-}
+            
+        );
+    }
+} 
 
 export default Comic;
